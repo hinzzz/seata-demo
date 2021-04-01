abfd92c38fa90e7132612b725791d83cae7b64da
hinzzz <157957329@qq.com>
2021-04-01 17:56:27 +0800

    seata 客户端初始化

---
+++ account/src/test/java/com/hinz/seata/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinz.seata.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ order/src/main/java/com/hinz/seata/order/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.hinz.seata.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    /*@GetMapping("/create")
+    public R create(){
+        return R.ok();
+    }*/
+}
